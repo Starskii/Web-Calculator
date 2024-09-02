918e5c9cf9b9a817fac3580d5d35c80975cb8701
Starskii <jakeylakie@yahoo.com>
2024-09-01 22:32:11 -0700

    created operate function and added logic based on operand

--- index.js
+++ index.js
@@ -14,22 +14,25 @@ const divide = (a, b) => {
     return a / b;
 }
 
-const operate = (a, b, operator) => {
-
+const operate = (operator, a, b) => {
+    let activeFunction = null;
+    switch(operator){
+        case "+":
+            activeFunction = add;
+            break;
+        case "-":
+            activeFunction = subtract;
+            break;
+        case "x":
+            activeFunction = multiply;
+            break;
+        case "/":
+            activeFunction = divide;
+            break;
+    }
+    let output = activeFunction(a, b);
 }
 
 let firstValue = 0;
 let secondValue = 0;
 let operand = null;
-
-const numberContainerNode = document.querySelector("#number-container");
-
-for (let i = 1; i < 11; i++){
-    const button = document.createElement("button");
-    button.classList.add("number-button");
-    if(i === 10)
-        button.innerHTML = 0;
-    else
-        button.innerHTML = i;
-    numberContainerNode.appendChild(button);
-}
