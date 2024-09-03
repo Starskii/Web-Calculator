bb6236b8a13222f715a2b1625aa63004ddcdbb8c
Starskii <jakeylakie@yahoo.com>
2024-09-02 17:27:41 -0700

    wip calculator

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const outputLabel = document.querySelector("#output");
+
 const add = (a, b) => {
     return a + b;
 }
@@ -30,9 +32,64 @@ const operate = (operator, a, b) => {
             activeFunction = divide;
             break;
     }
-    let output = activeFunction(a, b);
+    return output = activeFunction(a, b);
 }
 
+const updateOutput = (outputText) => {
+    outputLabel.innerHTML = outputText;
+}
+
+const buttonPressed = (e) => {
+    console.log(parseInt(e.target.innerHTML))
+    if(Number.isInteger(parseInt(e.target.innerHTML)))
+        processNumber(parseInt(e.target.innerHTML));
+    else{
+        processOperator(e.target.innerHTML);
+    }
+    updateOutput(`${outputValue}`)
+}
+
+const processNumber = (number) => {
+    if (operand === null){
+        firstValue = parseInt(firstValue.toString() + number.toString());
+        console.log(firstValue);
+    } else{
+        secondValue = parseInt(secondValue.toString() + number.toString());
+        console.log(secondValue);
+    }
+}
+
+const clearOperator = () => {
+    firstValue = null;
+    secondValue = null;
+    operand = null;
+    outputValue = defaultOutputValue;
+}
+
+const processOperator = (operator) => {
+    switch(operator){
+        case "C":
+            clearOperator();
+            break;
+        case "=":
+            console.log(operate(operand, firstValue, secondValue));
+            break;
+        case ".":
+            //Handle later
+            break;
+        default:
+            operand = operator;
+            break;
+    }
+}
+const defaultOutputValue = outputLabel.innerHTML;
+let outputValue = defaultOutputValue;
 let firstValue = 0;
 let secondValue = 0;
 let operand = null;
+
+const BUTTON_NODES = document.querySelectorAll(".input-button");
+
+for (button of BUTTON_NODES){
+    button.addEventListener("click", buttonPressed);
+}
